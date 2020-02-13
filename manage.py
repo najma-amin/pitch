938516e853219d3933a1083ccf6dbae9a1deff4a
najmaA <najmaamin10@gmai.com>
2020-02-13 12:07:06 +0300

    generated secret key

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
@@ -25,3 +26,4 @@ def make_shell_context():
 
 if __name__ == '__main__':
     manager.run()
+
