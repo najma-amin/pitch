d7a6367b5881a2768659a2b4f05f63aae2b28474
najmaA <najmaamin10@gmai.com>
2020-02-13 16:19:40 +0300

    email welcome user file added

--- app/Authh/views.py
+++ app/Authh/views.py
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "One Minute Pitch login"
+    title = "60seconds_pitch"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-export SECRET_KEY='najma'
+export SECRET_KEY='23099abf2e48eee828f0d9b38d4773873f29c8
+'
 export MAIL_USERNAME='sweetmum114@gmail.com'
-export MAIL_PASSWORD='*******'
+export MAIL_PASSWORD='hoyomacan@128'
 
 python manage.py server
